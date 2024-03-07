7f8b49aab1e5e0c2e456fdec6a3a58dec19b0991
De-Hype <iamsocialhype@gmail.com>
2024-03-06 17:16:29 -0800

    Added everything session in place and morgan

--- sibly-api/index.js
+++ sibly-api/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const session = require('express-session');
 const helmet = require("helmet");
+const morgan = require("morgan");
 const Connect = require("./src/config/db");
 
 
@@ -10,10 +11,21 @@ const app = express()
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"))
 app.use(express.json());
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    saveUnintialized:true,
+    resave:true,
+    cookie:{
+        httpOnly:true,
+        maxAge: parseInt( process.env.SESSION_MAX_AGE)
+    }
+}))
+
+
 const port = process.env.PORT || 7070
 
 Connect().then(()=>{
-
     app.listen(port, ()=>{console.log(`Server runing on ${port}`)})
 })
