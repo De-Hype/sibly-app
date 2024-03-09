8e5712fa97d18447895d97fb597b744583102b47
De-Hype <iamsocialhype@gmail.com>
2024-03-09 13:19:06 -0800

    set up stuff in the auth routes

---
+++ sibly-api/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const passport = require('passport');
+const { SignUp, LogOut, SignIn } = require("../controllers/auth.controller");
+const Limiter = require("../middleware/rateLimit");
+
+router.post('/register', Limiter, SignUp );
+router.get('/sign-in', Limiter, SignIn);
+router.get('/sign-out', LogOut);
+
+
+
+module.exports = router;
